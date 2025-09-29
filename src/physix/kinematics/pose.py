8f8dc4658a0b4664ed5f52f960f7fd05f8f33b5f
Mohammad Rahmani <mohammad.rahmani.xyz@gmail.com>
2025-09-29 23:46:37 +0200

    pose is added

--- src/physix/kinematics/pose.py
+++ src/physix/kinematics/pose.py
@@ -1,4 +1,8 @@
+from physix.kinematics.quaternion import Quaternion
+from physix.kinematics.position import Position
+
+
 class Pose:
-    def __init__(self, position, quaternion):
+    def __init__(self, position:Position, quaternion:Quaternion):
         self._position = position
         self._quaternion = quaternion
