a11df266cab16898a2198d48bdbc4b1b2195750c
Mohammad Rahmani <mohammad.rahmani.xyz@gmail.com>
2025-10-05 00:25:21 +0200

    unit update

--- src/physix/dimension/unit/unit.py
+++ src/physix/dimension/unit/unit.py
@@ -1,9 +1,9 @@
-from typing import Union
+from typing import Union, Optional
 
 from physix.dimension.dimension import Dimension
 
 
 class Unit:
-    def __init__(self, dimension:Dimension, id:Union[int,str]):
-        self._dimension = dimension
+    def __init__(self, id:Union[int,str], dimension: Optional[Dimension]=None):
         self._id = id
+        self._dimension = dimension
