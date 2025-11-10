203eb7e78ded7441834935ef4c88d50406040f92
Mohammad Rahmani <mohammad.rahmani.xyz@gmail.com>
2025-11-11 00:23:11 +0100

    supporting dimension unit is changed

--- src/physix/dimension/type/suppurting_dimension_units.py
+++ src/physix/dimension/type/suppurting_dimension_units.py
@@ -1,6 +1,6 @@
 class SupportingDimensionUnits:
     """
-    This class determines which dimension type is matchable with which units
+    This class determines which dimension kind is matchable with which units
     """
     def __init__(self, dimension_type:DimensionType, unit_types:List[UnitType]):
         self._dimension_type = dimension_type
