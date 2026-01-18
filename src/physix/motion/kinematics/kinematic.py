30714f5cbb3c5d7f360f5e4a0d88c158a99fdfe1
Mohammad Rahmani <mohammad.rahmani.xyz@gmail.com>
2026-01-18 22:24:35 +0100

    kinematics

--- src/physix/motion/kinematics/kinematic.py
+++ src/physix/motion/kinematics/kinematic.py
@@ -1,5 +1,5 @@
 class Kinematic(Vec):
     """
-    Doesnt have unit. maybe the data is normalized so for example position is unitless
+    Doesnt have unit. maybe the data_set is normalized so for example position is unitless
     """
     pass
